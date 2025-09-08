d0319ce6d6346d0bcea18ff4b0c7b323b9eb56b0
Jeff10B <jobeltran@estudiante.uniajc.edu.co>
2025-09-08 18:40:36 -0500

    Update Libro.java

--- src/Libro.java
+++ src/Libro.java
@@ -1,12 +1,12 @@
-// Libro.java
+
 public class Libro {
-    // Atributos privados para encapsulamiento
+    
     private String titulo;
     private String autor;
     private int numeroEjemplares;
     private int ejemplaresPrestados;
 
-    // Constructor por defecto
+    
     public Libro() {
     }
 
@@ -18,7 +18,7 @@ public class Libro {
         this.ejemplaresPrestados = ejemplaresPrestados;
     }
 
-    // Métodos Get y Set
+    
     public String getTitulo() {
         return titulo;
     }
@@ -51,25 +51,24 @@ public class Libro {
         this.ejemplaresPrestados = ejemplaresPrestados;
     }
 
-    // Método prestamo: Incrementa el número de ejemplares prestados
+    
     public boolean prestamo() {
         if (ejemplaresPrestados < numeroEjemplares) {
             ejemplaresPrestados++;
-            return true; // Préstamo exitoso
+            return true; 
         }
-        return false; // No hay ejemplares disponibles
+        return false; 
     }
 
-    // Método devolucion: Decrementa el número de ejemplares prestados
     public boolean devolucion() {
         if (ejemplaresPrestados > 0) {
             ejemplaresPrestados--;
-            return true; // Devolución exitosa
+            return true; 
         }
-        return false; // No hay ejemplares para devolver
+        return false;
     }
 
-    // Sobrescritura del método toString para imprimir la información del libro
+    
     @Override
     public String toString() {
         return "Título: " + titulo + ", Autor: " + autor +
